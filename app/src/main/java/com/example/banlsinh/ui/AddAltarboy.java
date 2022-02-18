81c35ca1ae771e337c00cce0561de89ceb5d4177
manhdzu <88848672+manhdzu@users.noreply.github.com>
2022-02-18 16:44:00 +0700

    BanLeSinhManage App

--- app/src/main/java/com/example/banlsinh/ui/AddAltarboy.java
+++ app/src/main/java/com/example/banlsinh/ui/AddAltarboy.java
@@ -1,4 +1,4 @@
-package com.example.banlsinh;
+package com.example.banlsinh.ui;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,11 +10,11 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.banlsinh.R;
 import com.example.banlsinh.custom.CustomToast;
 
 import java.util.HashMap;
@@ -35,14 +35,13 @@ public class AddAltarboy extends AppCompatActivity {
 
         context = getApplicationContext();
 
-        edt_tenthanh = (EditText) findViewById(R.id.them_tenthanh);
-        edt_hoten = (EditText) findViewById(R.id.them_hoten);
-        edt_ngaysinh = (EditText) findViewById(R.id.them_ngaysinh);
-        edt_lienlac = (EditText) findViewById(R.id.them_lienlac);
-        edt_diachi = (EditText) findViewById(R.id.them_diachi);
-        btn_cofirm = (Button) findViewById(R.id.button_them);
-        btn_cancel = (Button) findViewById(R.id.button_huy);
-
+        edt_tenthanh = findViewById(R.id.them_tenthanh);
+        edt_hoten = findViewById(R.id.them_hoten);
+        edt_ngaysinh = findViewById(R.id.them_ngaysinh);
+        edt_lienlac = findViewById(R.id.them_lienlac);
+        edt_diachi = findViewById(R.id.them_diachi);
+        btn_cofirm = findViewById(R.id.button_them);
+        btn_cancel = findViewById(R.id.button_huy);
     }
 
     @Override
@@ -51,9 +50,9 @@ public class AddAltarboy extends AppCompatActivity {
 
         btn_cofirm.setOnClickListener(view -> {
             getText();
-            if(type_tenthanh.isEmpty() || type_hoten.isEmpty() || type_diachi.isEmpty() || type_ngaysinh.isEmpty() || type_lienlac.isEmpty()){
+            if (type_tenthanh.isEmpty() || type_hoten.isEmpty() || type_diachi.isEmpty() || type_ngaysinh.isEmpty() || type_lienlac.isEmpty()) {
                 CustomToast.makeText(context, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT, CustomToast.WARNING, true).show();
-            }else{
+            } else {
                 AddAltarBoyFunc(urlInsert);
                 startActivity(new Intent(context, MainActivity.class));
             }
@@ -62,33 +61,33 @@ public class AddAltarboy extends AppCompatActivity {
         btn_cancel.setOnClickListener(view -> finish());
     }
 
-    private void AddAltarBoyFunc(String url){
+    private void AddAltarBoyFunc(String url) {
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 response -> {
-                    if(response.trim().equals("success")){
+                    if (response.trim().equals("success")) {
                         CustomToast.makeText(context, "Thêm thành công", Toast.LENGTH_SHORT, CustomToast.SUCCESS, true).show();
                         finish();
-                    }else
+                    } else
                         CustomToast.makeText(context, "Thêm thất bại", Toast.LENGTH_SHORT, CustomToast.ERROR, true).show();
                 },
-                error -> CustomToast.makeText(context, "Đã xảy ra lỗi", Toast.LENGTH_SHORT, CustomToast.ERROR, true).show()){
-                @Nullable
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("tenthanh", type_tenthanh);
-                    params.put("hoten", type_hoten);
-                    params.put("ngaysinh", type_ngaysinh);
-                    params.put("lienlac", type_lienlac);
-                    params.put("diachi", type_diachi);
-                    return params;
-                }
-            };
+                error -> CustomToast.makeText(context, "Đã xảy ra lỗi", Toast.LENGTH_SHORT, CustomToast.ERROR, true).show()) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("tenthanh", type_tenthanh);
+                params.put("hoten", type_hoten);
+                params.put("ngaysinh", type_ngaysinh);
+                params.put("lienlac", type_lienlac);
+                params.put("diachi", type_diachi);
+                return params;
+            }
+        };
         requestQueue.add(stringRequest);
     }
 
-    public void getText(){
+    public void getText() {
         type_tenthanh = edt_tenthanh.getText().toString().trim();
         type_hoten = edt_hoten.getText().toString().trim();
         type_ngaysinh = edt_ngaysinh.getText().toString().trim();
---
+++ app/src/main/java/com/example/banlsinh/ultility/NetworkChangeListener.java
@@ -0,0 +1,48 @@
+package com.example.banlsinh.ultility;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+import com.example.banlsinh.R;
+
+public class NetworkChangeListener extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (!Common.isConnectedToInternet((context))) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            View layout_diaglog = LayoutInflater.from(context).inflate(R.layout.check_internet, null);
+            builder.setView(layout_diaglog);
+
+            AppCompatButton btnRetry = layout_diaglog.findViewById(R.id.btn_retry);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            dialog.setCancelable(false);
+
+            Display display = ((WindowManager) context.getSystemService(context.WINDOW_SERVICE)).getDefaultDisplay();
+            int dialog_height = display.getHeight();
+            int dialog_width = display.getWidth();
+
+            dialog.getWindow().setGravity(Gravity.CENTER);
+            dialog.getWindow().setLayout((20 * dialog_width) / 27, (200 * dialog_height) / 457);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            btnRetry.setOnClickListener(view -> {
+                dialog.dismiss();
+                onReceive(context, intent);
+            });
+        }
+    }
+}
